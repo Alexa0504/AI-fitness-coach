871a621876bb22b6a64d4e36979ee20f350c778b
Kovacs Kinga Kendra <kingakendra.kovacs@gmail.com>
2025-10-31 16:09:59 +0100

    remove unused Link import from LoginPage and update dependencies

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import AuthForm from "../components/AuthForm";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import ThemeSwitcher from "../components/ThemeSwitcher";
 
 const LoginPage: React.FC = () => {
