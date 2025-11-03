2adcab3b500aa55727935163fffabbe982a2d9ca
Kovacs Kinga Kendra <kingakendra.kovacs@gmail.com>
2025-11-03 21:28:16 +0100

    update token_required decorator for user validation

---
+++ backend/utils/auth_decorator.py
@@ -0,0 +1,29 @@
+from functools import wraps
+from flask import request, jsonify
+from backend.models import User
+from backend.utils.security_utils import decode_auth_token
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+
+        if 'Authorization' in request.headers:
+            token = request.headers['Authorization'].split(" ")[1]
+
+        if not token:
+            return jsonify({"message": "Token is missing!"}), 401
+
+        user_id = decode_auth_token(token)
+
+        if not user_id:
+            return jsonify({"message": "Token is invalid or expired!"}), 401
+
+
+        user = User.query.get(user_id)
+        if not user:
+            return jsonify({"message": "User not found!"}), 404
+
+        return f(current_user=user, *args, **kwargs)
+
+    return decorated
