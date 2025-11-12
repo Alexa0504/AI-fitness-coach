e7714addd1ebde6eab19923de74f073cc45203dd
Zilahi Alexandra <zilahi.alexandra04@gmail.com>
2025-11-12 20:39:23 +0100

    swap Goals section above AI Plan on homepage

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -114,11 +114,11 @@ const HomePage: React.FC = () => {
 
                     <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
                         <div className="lg:col-span-2 space-y-8">
-                            <DashboardSection title="AI Workout and Diet Plan">
-                                <AiPlanCard/>
-                            </DashboardSection>
                             <DashboardSection title="Your Goals and Progress">
                                 <GoalsCard/>
+                            </DashboardSection>
+                            <DashboardSection title="AI Workout and Diet Plan">
+                                <AiPlanCard/>
                             </DashboardSection>
                         </div>
 
