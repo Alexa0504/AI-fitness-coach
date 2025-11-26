4a17be03e684d39e71eca338767400662413d82f
Tóth Tamás <t.tamas3208@gmail.com>
2025-11-26 15:35:34 +0100

    implement ProtectedRoute element

---
+++ client/src/utils/ProtectedRoute.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+
+interface Props {
+  children: JSX.Element;
+}
+
+const ProtectedRoute: React.FC<Props> = ({ children }) => {
+  const isAuthenticated = !!localStorage.getItem("token"); 
+
+  return isAuthenticated ? children : <Navigate to="/login" />;
+};
+
+export default ProtectedRoute;
